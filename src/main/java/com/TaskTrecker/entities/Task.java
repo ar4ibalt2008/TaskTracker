758f89c312a7b781e805057b7a0b80335cc15594
Arturio <arturovidal200088@gmail.com>
2026-06-06 03:19:52 +0200

    added entities

---
+++ src/main/java/com/TaskTrecker/entities/Task.java
@@ -0,0 +1,33 @@
+package com.TaskTrecker.entities;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private boolean completed;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+    public Task(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.completed = false;
+    }
+
+}
---
+++ src/main/java/com/TaskTrecker/entities/User.java
@@ -0,0 +1,31 @@
+package com.TaskTrecker.entities;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private String email;
+
+    @OneToMany(mappedBy = "User")
+    private List<Task> tasks;
+
+
+    public User(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+
+}
